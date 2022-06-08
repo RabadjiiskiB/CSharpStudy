609c1e2272a7ac59452d18a19b01b2f1938254a2
Borislav405 <rabadjiiskib@gmail.com>
2022-06-08 23:35:44 +0300

    Changed framework version from 5.0 to 6.0

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace CSharpStudy
+﻿Console.WriteLine("Ohayo");
+Console.WriteLine("Type a number to multiply by 2");
+int x = int.Parse(Console.ReadLine());
+while (x < 0)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    Console.WriteLine("Don't use negative numbers");
+    x = int.Parse(Console.ReadLine());
 }
+int y = 2;
+Console.WriteLine(x *= y);
