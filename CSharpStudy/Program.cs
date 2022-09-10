76e965cc94d3b06ed8e3fb1d74e7fd859c9469a2
Borislav405 <rabadjiiskib@gmail.com>
2022-09-10 20:21:32 +0300

    idk

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -1,18 +1,4 @@
-﻿ushort n1 = 52130;
-sbyte n2 = -115;
-int n3 = 4825932;
-sbyte n4 = 97;
-short n5 = -10000;
-short n6 = 20000;
-byte n7 = 224;
-int n8 = 970700000;
-sbyte n9 = 112;
-sbyte n10 = -44;
-int n11 = -1000000;
-short n12 = 1990;
-long n13 = 123456789123456789;
-
-Console.WriteLine("Ohayo");
+﻿Console.WriteLine("Welcome");
 Console.WriteLine("Type 1 for multiplication");
 int nav = int.Parse(Console.ReadLine());
 while (nav == 0)
@@ -29,9 +15,19 @@ while (x < 0)
     Console.WriteLine("Don't use negative numbers");
     x = int.Parse(Console.ReadLine());
 }
-int y = 2;  
+int y = 2;
 Console.WriteLine(x *= y);
 }
 
-bool equal = Math.Abs(n12 - n10 ) < 0.000001;
-Console.WriteLine(equal);
+if(nav == 2)
+{
+    bool equal = Math.Abs(n12 - n10 ) < 0.000001;
+    Console.WriteLine(equal);
+}
+
+if(nav == 3)
+{
+    int test = 0x256;
+    Console.WriteLine(test);
+}
+
